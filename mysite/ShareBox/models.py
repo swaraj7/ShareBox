d7c62b6986eb649cb6b2e3edd3121d8a70a4d01c
Saurav Swaraj <saurav.swaraj@grofers.com>
2016-06-11 18:31:21 +0530

    changed database settings

--- mysite/ShareBox/models.py
+++ mysite/ShareBox/models.py
@@ -1,5 +1,8 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class snippet(models.Model):
+    snippet_name = models.CharField(max_length=20)
+    snippet_privacy = models.BooleanField(default=False)
+    snippet_expiry = models.CharField(max_length=3)
+    snippet_file = models.CharField(max_length=1000)
