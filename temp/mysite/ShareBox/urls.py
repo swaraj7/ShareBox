bf3b9b7927156ea1ba0fb082dee7da7f85931edf
Saurav Swaraj <saurav.swaraj@grofers.com>
2016-06-14 12:02:14 +0530

    removed irrelevant files and updated view.py

--- temp/mysite/ShareBox/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^([ \w]+)', views.send_data),
-    url(r'^$', views.index, name='index'),
-]
--- temp/mysite/ShareBox/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-import requests
-import httplib
-
-
-def send_data(request, snippet_data):
-    pass
